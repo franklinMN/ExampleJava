85908c85203387b8c6dead8ad932dd96b223b84f
Franklin Maria Nayagam <56641239+franklinMN@users.noreply.github.com>
2024-01-21 06:15:35 +0000

    done

--- Inmakes/01.loops/ReverseNumber.java
+++ Inmakes/01.loops/ReverseNumber.java
@@ -1,18 +1,15 @@
 class ReverseNumber {
-
-
     public static void main( String[] args ) {
 
 
-        int number = 153;
+        int number = 123456789;
         int reversedNumber=0;
-        int quo=0, rem=0;
+        int quo=number, rem=0;
 
         while(quo!=0) {
-            quo = number / 10;
-            rem = number % 10;
-            reversedNumber += rem*10;
-            number = number / 10;
+            rem = quo % 10;
+            reversedNumber = reversedNumber*10 + rem;
+            quo = quo / 10;
         }
 
         System.out.println("Original Number : " + number);
