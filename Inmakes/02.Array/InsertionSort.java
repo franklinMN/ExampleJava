fadc1359174f0cece483df47dcad3827ab8cf48b
Franklin Maria Nayagam <56641239+franklinMN@users.noreply.github.com>
2024-01-22 20:26:57 +0530

    done

---
+++ Inmakes/02.Array/InsertionSort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    public static void insertionSort(int[] arr){
+
+        //1. key
+        //2. position before key
+        //3. Move elements greater than the selected element to the right
+        //4. Insert the selected element in its correct position
+
+    }
+    public static void main(String[] args) {
+
+        int[] array = {4, 2, 7, 1, 9};
+
+        //sorting the array
+        Arrays.insertionSort(array);
+
+        //
+    }
+    
+}
