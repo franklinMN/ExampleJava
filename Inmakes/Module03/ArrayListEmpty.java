3a098ce81307278606f215f14dc7d3a249b6c9c6
Franklin Maria Nayagam <56641239+franklinMN@users.noreply.github.com>
2024-01-26 10:41:09 +0530

    done

---
+++ Inmakes/Module03/ArrayListEmpty.java
@@ -0,0 +1,8 @@
+// 2. Write A Java Program To Test An ArrayList Is Empty Or Not
+
+public class ArrayListEmpty {
+    public static void main(String[] args) {
+        List<Integer> arrayList= new ArrayList<>();
+    }
+    
+}
