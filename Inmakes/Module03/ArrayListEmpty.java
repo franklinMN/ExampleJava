790ed641411491b29818318329048fb2e2f33259
Franklin Maria Nayagam <56641239+franklinMN@users.noreply.github.com>
2024-01-26 16:45:10 +0530

    done

--- Inmakes/Module03/ArrayListEmpty.java
+++ Inmakes/Module03/ArrayListEmpty.java
@@ -1,8 +1,31 @@
 // 2. Write A Java Program To Test An ArrayList Is Empty Or Not
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class ArrayListEmpty {
     public static void main(String[] args) {
-        List<Integer> arrayList= new ArrayList<>();
+        List<Integer> arrayList = new ArrayList<>();
+
+        arrayList.add(123);
+        arrayList.add(456);
+        arrayList.add(789);
+
+        System.out.println("Array Elements - " + arrayList);
+
+        int sum = 0;
+
+        for (Integer value : arrayList) {
+            sum += value; // auto unboxing example
+        }
+        System.out.println("Sun of the array elements - " + sum);
+
+        System.out.print(arrayList + " - ");
+        System.out.println(arrayList.isEmpty() ? "Array is Empty" : "Array has elements");
+
+        arrayList.clear();
+        System.out.print(arrayList + " - ");
+        System.out.println(arrayList.isEmpty() ? "Array is Empty" : "Array has elements");
     }
-    
+
 }
