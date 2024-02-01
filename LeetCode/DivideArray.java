25d317e8ee5568ac8bda495762e613d4482ae095
Franklin Maria Nayagam <56641239+franklinMN@users.noreply.github.com>
2024-02-01 11:56:20 +0530

    done

---
+++ LeetCode/DivideArray.java
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/divide-array-into-arrays-with-max-difference/?envType=daily-question&envId=2024-02-01
+
+public class Solution {
+    public int[][] divideArray(int[] nums, int k) {
+        List<List<Integers>> result = new ArrayList<>(nums);
+    }
+}
+
+public class DivideArray {
+    public static void main(String[] args) {
+
+    }
+
+}
