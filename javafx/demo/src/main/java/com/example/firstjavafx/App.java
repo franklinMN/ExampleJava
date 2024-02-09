9a88d549ecef7597ff3bc769948b3c85ad9b7434
Franklin Maria Nayagam <56641239+franklinMN@users.noreply.github.com>
2024-02-09 09:15:23 +0530

    firstjavafx updated

--- javafx/demo/src/main/java/com/example/firstjavafx/App.java
+++ javafx/demo/src/main/java/com/example/firstjavafx/App.java
@@ -6,29 +6,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 /**
  * JavaFX App
  */
 public class App extends Application {
 
-    private static Scene scene;
-
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
---
+++ javafx/demo/src/main/java/com/example/firstjavafx/MainControler.java
@@ -0,0 +1,34 @@
+package com.example.firstjavafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.shape.Circle;
+
+public class MainControler {
+
+    @FXML
+    private Circle circle;
+    private double x;
+    private double y;
+
+    public void up(ActionEvent e) {
+        // System.out.println("UP");
+        circle.setCenterY(y -= 10);
+    }
+
+    public void down(ActionEvent e) {
+        // System.out.println("down");
+        circle.setCenterY(y += 10);
+    }
+
+    public void left(ActionEvent e) {
+        // System.out.println("left");
+        circle.setCenterX(x -= 10);
+    }
+
+    public void right(ActionEvent e) {
+        // System.out.println("right");
+        circle.setCenterX(x += 10);
+    }
+
+}
