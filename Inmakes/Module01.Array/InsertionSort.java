19a2fe74ae8ae12c1b2280508b5c3fe9f66e3459
Franklin Maria Nayagam <56641239+franklinMN@users.noreply.github.com>
2024-02-17 10:00:56 +0530

    done

--- Inmakes/Module01.Array/InsertionSort.java
+++ Inmakes/Module01.Array/InsertionSort.java
@@ -2,22 +2,43 @@ import java.util.Arrays;
 
 public class InsertionSort {
 
-    public static void insertionSort(int[] arr){
+    public static void insertionSort(int[] arr) {
 
-        //1. key
-        //2. position before key
-        //3. Move elements greater than the selected element to the right
-        //4. Insert the selected element in its correct position
+        // 1. First element is always sorted.
+        // 2. Start from the second element.
+        // 3. Select the key element.
+        // 4. Compare with the elements to the left og the key.
+        // 5. Arrange in ascending order.
+        // 6. Increament the key and repeat the process.
+
+        int key;
+
+        for (int i = 1; i < arr.length; i++) {
+            key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && key < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+
+        }
 
     }
+
     public static void main(String[] args) {
 
-        int[] array = {4, 2, 7, 1, 9};
+        int[] array = { 4, 2, 7, 1, 9 };
 
-        //sorting the array
-        Arrays.insertionSort(array);
+        // sorting the array
+        insertionSort(array);
 
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+
+        }
         //
     }
-    
+
 }
--- Inmakes/Module01.Array/RemoveDuplicates.java
+++ Inmakes/Module01.Array/RemoveDuplicates.java
@@ -5,21 +5,22 @@ import java.util.Arrays;
 class RemoveDuplicates {
 
     public static int[] removeDuplicatesFromArray(int[] array) {
-        
+
         Arrays.sort(array);
 
         int uniqueCount = 1;
-        
-        for( int i=1 ; i<array.length ; i++ ){
-            if( array[i] != array[i-1])
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] != array[i - 1])
                 array[uniqueCount++] = array[i];
         }
 
         return Arrays.copyOf(array, uniqueCount);
     }
+
     public static void main(String[] args) {
-        
-        int[] array = { -1, 999, 0, -999, -9999, 9999, -999, -9999, 9999, -1, 999, 0};
+
+        int[] array = { -1, 999, 0, -999, -9999, 9999, -999, -9999, 9999, -1, 999, 0 };
         System.out.println("Original array : " + Arrays.toString(array));
 
         int[] uniqueArray = removeDuplicatesFromArray(array);
